4b9929103716ae09771495af68a6202724ef7635
Karl Pickett <kjp@uchicago.edu>
2018-07-04 16:11:39 +0000

    s

--- bash/karlcommon.sh
+++ bash/karlcommon.sh
@@ -5,6 +5,12 @@ export VIMINIT='so ~/.vimrc-karl'
 gvd() { git diff --patience "$@" ; }
 gvds() { git diff --patience --staged "$@" ; }
 
+reload() {
+    echo "Reloading ~/.profile"
+    . ~/.profile
+}
+
+alias ta='tmux a'
 alias vi=vim
 alias ls='ls --color=auto -I __pycache__'
 alias tree='tree -I __pycache__'
